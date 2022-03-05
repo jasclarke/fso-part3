baac462ccef30b9b4abf06c59f2d93a3a51c4aaf
Jason Clarke <61100900+jasclarke@users.noreply.github.com>
2022-03-04 20:49:55 -0400

    Completed exercise 3.8 using token feature

--- index.js
+++ index.js
@@ -5,17 +5,14 @@ const morgan = require('morgan')
 const app = express()
 const PORT = 3001
 
+morgan.token('post-request', (request) => {
+    return JSON.stringify(request.body)
+})
+
 app.use(express.json())
-app.use(morgan((tokens, request, response) => {
-    return [
-        tokens.method(request, response),
-        tokens.url(request, response),
-        tokens.status(request, response),
-        tokens.res(request, response, 'content-length'), '-',
-        tokens['response-time'](request, response), 'ms',
-        JSON.stringify(request.body)
-    ].join(' ')
-}))
+app.use(morgan(
+    ':method :url :status :res[content-length] - :response-time ms :post-request'
+))
 
 let contacts = [
     { 
